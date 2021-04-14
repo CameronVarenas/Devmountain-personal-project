b445de0a6620e71163f097f791f1506f8059107e
Cameron Varenas <camerontvarenas@gmail.com>
2021-04-14 14:38:26 -0400

    Created landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Landing from './components/Landing'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='header'>
+        <section className='header-text'>
+          <h1 className='title'>Kioku</h1>
+          <h3 className='options'>Home</h3>
+          <h3 className='options'>About</h3>
+          <h3 className='auth'>Log In</h3>
+        </section>
       </header>
+      <div className='page-display'>
+        <Landing />
+      </div>
     </div>
   );
 }
---
+++ src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Landing() {
+    return (
+            <section className='landing-content'>
+                <h1>Welcome to Kioku!</h1>
+                <p>Kioku is a spaced repitition flashcard app.</p>
+                <p>Please log in or register to get started.</p>
+            </section>
+    )
+}
