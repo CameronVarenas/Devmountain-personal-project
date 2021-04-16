3c6e9bb594195d03743608c320c2fdeaa087f403
Cameron Varenas <camerontvarenas@gmail.com>
2021-04-16 14:19:25 -0400

    Finished auth component display

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -6,17 +6,31 @@ export default function Auth() {
             <h3>Login</h3>
             <section>
                 <input className='auth-input' placeholder='Username or Email'></input>
-                <input className='auth-input' placeholder='Password'></input>
-                <button>Login</button>
-                <p>Forgot Password?</p>
             </section>
+            <section>
+                <input className='auth-input' placeholder='Password'></input>
+            </section>
+            <section className='login-options'>
+                <button className='login-button'>Login</button>
+                <p className='forgot-pass'>Forgot Password?</p>
+            </section>
+
             <p>Or</p>
+
             <h3>Register Now</h3>
             <section>
                 <input className='auth-input' placeholder='Username'></input>
+            </section>
+            <section>
                 <input className='auth-input' placeholder='Email Address'></input>
+            </section>
+            <section>
                 <input className='auth-input' placeholder='Password'></input>
+            </section>
+            <section>
                 <input className='auth-input' placeholder='Re-Type Password'></input>
+            </section>
+            <section className='register-button'>
                 <button>Register</button>
             </section>
         </div>
