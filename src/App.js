55c0e6c16e1befd8e921298dd6c5c3071b7e9381
Cameron Varenas <camerontvarenas@gmail.com>
2021-04-19 11:12:10 -0400

    Installed massive and dotenv

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, {Component} from 'react';
+import {HashRouter} from 'react-router-dom';
 import './App.css';
 import Landing from './components/Landing';
 import Auth from './components/Auth';
@@ -6,27 +8,37 @@ import Flashcard from './components/Flashcard';
 import AddCardToDeck from './components/AddCardToDeck';
 import CardList from './components/CardList'
 
-function App() {
-  return (
-    <div className="App">
-      <header className='header'>
-        <section className='header-text'>
-          <h1 className='title'>Kioku</h1>
-          <h3 className='options'>Home</h3>
-          <h3 className='options'>About</h3>
-          <h3 className='login'>Log In</h3>
-        </section>
-      </header>
-      <div className='page-display'>
-        {/* <Landing /> */}
-        {/* <Auth /> */}
-        {/* <UserDecks /> */}
-        {/* <Flashcard /> */}
-        {/* <AddCardToDeck /> */}
-        {/* <CardList /> */}
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className='header'>
+          <section className='header-text'>
+            <h1 className='title'>Kioku</h1>
+            <h3 className='options'>Home</h3>
+            <h3 className='options'>About</h3>
+            <h3 className='login'>Log In</h3>
+          </section>
+        </header>
+        <div className='page-display'>
+          <Landing />
+          {/* <Auth /> */}
+          {/* <UserDecks /> */}
+          {/* <Flashcard /> */}
+          {/* <AddCardToDeck /> */}
+          {/* <CardList /> */}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default App;
+export default App
