784c84bbf9463cbffd8b76579fba1dac3b74f795
Cameron Varenas <camerontvarenas@gmail.com>
2021-04-20 13:49:19 -0400

    Installed express-session

--- server/index.js
+++ server/index.js
@@ -1,10 +1,40 @@
-const express = require('express');
+require('dotenv').config();
+const express = require('express'),
+      authController = require('./controllers/authController'),
+      cardController = require('./controllers/cardController'),
+      decksController = require('./controllers/decksController'),
+      massive = require('massive'),
+      session = require('express-session');
+
+const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env;
 
 const app = express();
 
+app.use(express.json());
 
-
-const port = 5050;
-app.listen(port, () => {
-    console.log(`Server listening on port: ${port}`);
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+}).then(db => {
+    app.set('db', db);
+    console.log('db connected');
 })
+
+app.use(
+    session({
+        resave: true,
+        saveUninitialized: false,
+        secret: SESSION_SECRET,
+        cookie: {maxAge: 1000 * 60 * 60 * 24 * 365},
+    })
+);
+
+//Auth Endpoints
+
+//Card Endpoints
+
+//Deck Endpoints
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Server listening on port: ${SERVER_PORT}`);
+});
