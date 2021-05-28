5ddf56ae737c6a7e41cd434eaa10d9db408d73b6
Cameron Varenas <camerontvarenas@gmail.com>
2021-05-28 16:42:42 -0400

    Updated MappedDecks.js to use useState for deck rename input

--- src/components/MappedDecks.js
+++ src/components/MappedDecks.js
@@ -1,58 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-class MappedDecks extends Component {
-    constructor() {
-        super();
-        this.state = {
-            deckNewName: ''
-        }
-    }
+export default function MappedDecks(props) {
+    const [deckNewName, setDeckNewName] = useState('');
 
-    handleDeckNewNameInput(e) {
-        this.setState({deckNewName: e.target.value});
+    const handleDeckNewNameInput = (e) => {
+        setDeckNewName(e.target.value);
     }
-
-    render() {
-        return (
-            <section className='deck-display'>
-            <div>
-                <h5>{this.props.deck.name}</h5>
-            </div>
-            <div>
-                <Link
-                    to={`/flashcard/${this.props.deck.deck_id}`}
-                    className='deck-options'
-                >Study</Link>
-                <Link
-                    to={`/add-card/${this.props.deck.deck_id}`}
-                    className='deck-options'
-                >Add Card</Link>
-                <Link
-                    to={`/card-list/${this.props.deck.deck_id}`}
-                    className='deck-options'
-                >View Cards</Link>
-                <button
-                    className='deck-options'
-                    onClick={() => {
-                        this.props.renameDeck(this.props.deck.deck_id, this.state.deckNewName)
-                        this.setState({deckNewName: ''})
-                    }}
-                >Rename Deck</button>
-                <button
-                    className='deck-options'
-                    onClick={() => this.props.deleteDeck(this.props.deck.deck_id)}
-                >Delete Deck</button>
-            </div>
-            <input
-                type='text'
-                placeholder='Enter a new name'
-                onChange={e => this.handleDeckNewNameInput(e)}
-                value={this.state.deckNewName}
-            ></input>
-            </section>
-        )
-    }
-}
-
-export default MappedDecks;
+    console.log(deckNewName)
+    return (
+        <section className='deck-display'>
+        <div>
+            <h5>{props.deck.name}</h5>
+        </div>
+        <div>
+            <Link
+                to={`/flashcard/${props.deck.deck_id}`}
+                className='deck-options'
+            >Study</Link>
+            <Link
+                to={`/add-card/${props.deck.deck_id}`}
+                className='deck-options'
+            >Add Card</Link>
+            <Link
+                to={`/card-list/${props.deck.deck_id}`}
+                className='deck-options'
+            >View Cards</Link>
+            <button
+                className='deck-options'
+                onClick={() => props.renameDeck(props.deck.deck_id, deckNewName)}
+            >Rename Deck</button>
+            <button
+                className='deck-options'
+                onClick={() => props.deleteDeck(props.deck.deck_id)}
+            >Delete Deck</button>
+        </div>
+        <input
+            type='text'
+            placeholder='Enter a new name'
+            onChange={e => handleDeckNewNameInput(e)}
+            value={deckNewName}
+        ></input>
+        </section>
+    )
+};
